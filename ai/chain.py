52bb1aabbd4e5ffd0ecf2830d2410e7797d8bbb9
sandip <sandipshahipun4@gmail.com>
2026-08-17 22:44:46 +0545

    Added ask_question file by sepreating the file handle and ask-ai

--- ai/chain.py
+++ ai/chain.py
@@ -89,44 +89,6 @@ async def upload_pdf(
             detail={"error":str(e)}
         )
 
-@router.post("/ask-ai")
-async def ask_question(
-    request : UserQuestion,
-    current_user: User = Depends(get_current_user),
-    db: Session = Depends(get_db)
-): 
-
-    if not request.question.strip():
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Question cannot be empty",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
-    try: 
-        #pass the question to the llm 
-        answer =rag_chain.invoke({"question": request.question})
-
-        #save the entire chat to the database
-        chat_message = ChatMessage(
-            user_id = current_user.id,
-            question = request.question,
-            answer = answer,
-        )
-        db.add(chat_message)
-        db.commit()
-        db.refresh(chat_message)
-
-        return {
-            "question": request.question,
-            "answer": answer,
-            "message_id": chat_message.id
-        }
-
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f" AI Processed Failed: {str(e)}"
-        )
 
 @router.delete("/delete-pdf/{document_id}")
 async def delete_pdf(
--- main.py
+++ main.py
@@ -5,14 +5,15 @@ from fastapi.exceptions import RequestValidationError
 from routers.users import router as user_router
 from ai.chain import router as ai_router
 from chat.history import router as history_router
+from ai.ask_question import router as ask_router
 
 app = FastAPI()
 
 app.include_router(auth_router, prefix="/api/auth", tags=["Authentication"])
 app.include_router(user_router, prefix="/api/user", tags=["Users"])
-app.include_router(ai_router, prefix="/api/ai", tags=["AI"])
+app.include_router(ai_router, prefix="/api/ai", tags=["PDF File handle"])
 app.include_router(history_router, prefix="/api/history", tags=["History Cleaner"])
-
+app.include_router(ask_router, prefix="/api/ai", tags=["ASK-AI"])
 app.add_exception_handler(HTTPException, HTTPException_handler)
 app.add_exception_handler(RequestValidationError, validation_errorh_handler)
 app.add_exception_handler(Exception, unexpected_error_handler)
